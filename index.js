bdcc3f65f5def7877622e0a6d1a91bdc7f7f8042
rgibbons131 <richiegibbons99@gmail.com>
2023-06-20 21:31:31 -0600

    init repo

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require("express");
+const dotenv = require("dotenv");
+const MongoClient = require("mongodb").MongoClient;
+const mongodb = require("./db/connect");
+// const elements = require("./routes/elements.js");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const { auth } = require("express-openid-connect");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: "https://elements-richard.onrender.com",
+  clientID: "DciNcQ4tiRf3MyaFzGCd94yuWOIv4BOu",
+  issuerBaseURL: "https://dev-c05tuxzqaokwihrb.us.auth0.com",
+};
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8080;
+
+app
+  .use(bodyParser.json())
+  .use(cors())
+  .use(auth(config))
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  })
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
